4892f25a2836d21bcb30b2ca9c135e1e59665293
n4t1 <n4t1996@gmail.com>
2019-12-17 20:08:20 +0100

    fix: fix main component styles, create noDataPipe

--- src/app/_shared/pipes/shared-pipes.module.ts
+++ src/app/_shared/pipes/shared-pipes.module.ts
@@ -4,12 +4,14 @@ import { ConvertAndFormatTimePipe } from './convert-and-format-time/convertAndFo
 import { ConvertAndFormatDatePipe } from './convert-and-format-date/convertAndFormatDate.pipe';
 import { SetMoviePlayLangPipe } from './set-movie-play-lang/setMoviePlayLang.pipe';
 import { SetMoviePlayViewPipe } from './set-movie-play-view/setMoviePlayView.pipe';
+import { NoDataPipe } from './no-data/no-data.pipe';
 
 const pipes = [
   ConvertAndFormatTimePipe,
   ConvertAndFormatDatePipe,
   SetMoviePlayLangPipe,
-  SetMoviePlayViewPipe
+  SetMoviePlayViewPipe,
+  NoDataPipe
 ];
 
 @NgModule({
